8fa448b131ec469919d94a35d1d95600bff4dad5
Yasin Onur <onur23@itu.edu.tr>
2026-03-12 23:37:01 +0300

    Initial Commit

---
+++ silent-chat/silent_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  silent_chatApp.swift
+//  silent-chat
+//
+//  Created by Yasin Onur on 12.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct silent_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
